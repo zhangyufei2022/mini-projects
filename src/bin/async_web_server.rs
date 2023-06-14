5078ae2721ec0c9910d4eb9469805f00e1324088
zyf <18362966889@163.com>
2023-06-14 11:36:58 +0800

    async web server

--- src/bin/async_web_server.rs
+++ src/bin/async_web_server.rs
@@ -1,58 +1,46 @@
 use std::{
     fs,
-    io::{BufRead, BufReader, Write},
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
-    thread,
     time::Duration,
 };
 
-fn main() {
+use async_std::task;
+
+#[async_std::main]
+async fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(3)
-        .build()
-        .unwrap();
 
     // listener.incoming 会在当前阻塞式监听
     // take 方法限制迭代的最大次数
     for stream in listener.incoming().take(5) {
         match stream {
             Ok(stream) => {
-                pool.spawn(|| {
-                    handle_connection(stream);
-                });
+                handle_connection(stream).await;
             }
             Err(err) => {
                 eprintln!("Failed to get tcpstream, reason:{}", err);
             }
         }
     }
-    println!("Shutting down...");
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buffer_reader = BufReader::new(&mut stream);
-    let request: Vec<_> = buffer_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    // println!("http request: {:#?}", request);
+async fn handle_connection(mut stream: TcpStream) {
+    println!("start...");
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-    let first_line = if let Some(first_line) = request.get(0) {
-        first_line
-    } else {
-        return;
-    };
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     // 访问根或者/sleep路径，显示hello.html页面；访问其他路径，显示err.html
-    let (code, filename) = match &first_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 Not Found", "err.html"),
+    let (code, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        task::sleep(Duration::from_secs(5)).await;
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 Not Found", "err.html")
     };
 
     let contents = if let Ok(contents) = fs::read_to_string(filename) {
@@ -69,4 +57,5 @@ fn handle_connection(mut stream: TcpStream) {
             eprintln!("Failed to respond, reason:{}", err);
         }
     }
+    println!("finish...");
 }
